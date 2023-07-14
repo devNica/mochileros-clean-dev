49c87370a4395274c4972137d2295d91db0519ae
devNica <designsdevnicaragua@gmail.com>
2023-07-14 15:55:42 -0600

    :recycle: refactorizar module de Autenticacion de usuarios | servicio de encryptacion de password y persistencia de datos de usuario (auth module)

--- src/authupd/adapters/secondary/repositories/useraccount_repository.ts
+++ src/authupd/adapters/secondary/repositories/useraccount_repository.ts
@@ -1,13 +1,37 @@
 import { UserRegisterDAOModel } from '@authupd/models/dao/useraccount_dao.model'
 import { UserRegisterDTOModel } from '@authupd/models/dto/useraccount_dto.model'
 import { InsertUserOutputPort } from '@authupd/ports/output/insertuser_output.port'
+import UserAccountModel from '@infrastructure/sequelize/models/UserAccountModel'
+import UserProfileModel from '@infrastructure/sequelize/models/UserProfileModel'
+import sequelizeInstance from '@infrastructure/sequelize/sequelizeConfig'
 
 export class UserAccounRepositoryAdapter implements InsertUserOutputPort {
   async createUser (newUser: UserRegisterDTOModel): Promise<UserRegisterDAOModel> {
     console.log(newUser)
+
+    let user = {
+      id: '',
+      createdAt: ''
+    }
+
+    await sequelizeInstance.transaction(async (t) => {
+      user = await UserAccountModel.create({
+        email: newUser.email,
+        password: newUser.passwordHash,
+        phoneNumber: newUser.phoneNumber,
+        isRoot: false,
+        fk_status: newUser.userAccountStatusId
+      }, { transaction: t })
+
+      await UserProfileModel.create({
+        fk_user: user.id,
+        fk_profile: newUser.profileId
+      }, { transaction: t })
+    })
+
     return {
-      userId: '0001-00999',
-      createdAt: '2023-07-14T20:50:22.102Z'
+      userId: user.id,
+      createdAt: user.createdAt
     }
   }
 }
--- src/authupd/usecases/userregister_usecase.ts
+++ src/authupd/usecases/userregister_usecase.ts
@@ -1,22 +1,28 @@
+import { UserAccountStatusMap, UserProfileMap } from '@auth/domain/models/maps'
 import { UserRegisterDTOModel } from '@authupd/models/dto/useraccount_dto.model'
 import { UserRegisterRequestModel } from '@authupd/models/request/useraccount_req.model'
 import { UserRegisterResponseModel } from '@authupd/models/response/useraccount_res.model'
 import { UserRegisterInputPort } from '@authupd/ports/input/userregister_input.port'
 import { InsertUserOutputPort } from '@authupd/ports/output/insertuser_output.port'
+import { PasswordEncryptorOutputPort } from '@core/ports/output/service/encryptors/password_encryptor_output.port'
 
 export class UserRegisterUsecase implements UserRegisterInputPort {
   constructor (
-    private readonly port: InsertUserOutputPort
+    private readonly repository: InsertUserOutputPort,
+    private readonly encryptor: PasswordEncryptorOutputPort
+
   ) {}
 
   async exec (data: UserRegisterRequestModel): Promise<UserRegisterResponseModel> {
     const newUSer: UserRegisterDTOModel = {
       email: data.email,
-      passwordHash: data.password,
-      phoneNumber: data.phoneNumber
+      passwordHash: await this.encryptor.derivePassword(data.password),
+      phoneNumber: data.phoneNumber,
+      userAccountStatusId: UserAccountStatusMap.unverifiableIdentity,
+      profileId: UserProfileMap.customers
     }
 
-    const user = await this.port.createUser(newUSer)
+    const user = await this.repository.createUser(newUSer)
     return {
       userId: user.userId,
       createdAt: user.createdAt
